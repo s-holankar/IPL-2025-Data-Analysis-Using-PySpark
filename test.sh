0ca27e5c9eb5c135e175c956d380a834e3357a09
Sonal Govind Holankar <56123579+s-holankar@users.noreply.github.com>
2025-05-31 19:41:33 +0530

    Update test.sh
    new code added

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 # pwd
 
-export PATH=/APP/anaconda3/bin:$PATH;export PYSPARK_PYTHON='/APP/anaconda3/bin/python3';export PYSPARK_DRIVER_PYTHON='/APP/anaconda3/bin/python3';/APP/SPARK/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --jars "/DATA/INBOUND/ANALYTICS/HOW/JDBC_JAR/ngdbc.jar,/home/sisr6adm/analytics/userapplications/P10060247/_Projects_Covid_/sqljdbc42-6.0.8112.jar" "/home/sisr6adm/analytics/userapplications/P55058192/_Projects_IPL_2025_/test2.py"
+export PATH=/APP/anaconda3/bin:$PATH;export PYSPARK_PYTHON='/APP/anaconda3/bin/python3';export PYSPARK_DRIVER_PYTHON='/APP/anaconda3/bin/python3';/APP/SPARK/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --jars "/DATA/INBOUND/ANALYTICS/HOW/JDBC_JAR/ngdbc.jar,/<your_folder_path>/_Projects_Python_/sqljdbc42-6.0.8112.jar" "/home/<your_folder_path>/_Projects_IPL_2025_/test.py"
  
 
-# ping nvmbdprp021365.jio.com:6080 
